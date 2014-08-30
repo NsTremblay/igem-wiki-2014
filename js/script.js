bfd2629d5dd460e06b3540350c6195487138bfa1
Nicolas <ns.tremblay@gmail.com>
2014-08-30 14:42:36 -0400

    Added a couple of files and changed some fonts use of js to navigate pages

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function changePage(link){
+	if(link=="triStable"){
+		window.location.href = 'tri-stable-switch.html';
+	}
+	if(link=="model"){
+		window.location.href = 'model.html';
+	}
+	if(link=="application"){
+		window.location.href = 'application.html';
+	}
+	if(link=="model"){
+		window.location.href = 'model.html';
+	}
+	if(link=="method"){
+		window.location.href = 'method.html';
+	}
+	if(link=="results"){
+		window.location.href = 'results.html';
+	}
+}
