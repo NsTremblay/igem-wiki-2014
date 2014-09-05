0f2ee92263737a0b7a9b9429b7311966386fcac3
Nicolas <ns.tremblay@gmail.com>
2014-09-05 13:36:02 -0400

    Inserted the HP materal temptative

--- js/script.js
+++ js/script.js
@@ -18,3 +18,50 @@ function changePage(link){
 		window.location.href = 'results.html';
 	}
 }
+
+function showSub(subItem){
+	if(subItem =="Modeling"){
+		var div = document.getElementById('modeling');
+		if(div.style.display=="block"){
+			div.style.display = "none";
+		}else{
+			div.style.display = "block";
+		}
+
+	}
+
+}
+
+//Here stc stands for subtochange
+function changeHP(stc){
+
+document.getElementById("lts").style.display = "none";
+document.getElementById("class").style.display = "none";
+document.getElementById("festival").style.display = "none";
+document.getElementById("uni").style.display = "none";
+document.getElementById("mini").style.display = "none";
+document.getElementById("ogem").style.display = "none";
+document.getElementById("youtube").style.display = "none";
+document.getElementById("book").style.display = "none";
+
+	if(stc=="lts"){
+		document.getElementById("lts").style.display = "block";
+	}else if(stc=="class"){
+		document.getElementById("class").style.display = "block";
+	}else if(stc=="festival"){
+		document.getElementById("festival").style.display = "block";
+	}else if(stc=="uni"){
+		document.getElementById("uni").style.display = "block";
+		getPieGraph();
+		getBarGraph();
+	}else if(stc=="mini"){
+		document.getElementById("mini").style.display = "block";
+	}else if(stc=="ogem"){
+		document.getElementById("ogem").style.display = "block";
+	}else if(stc=="youtube"){
+		document.getElementById("youtube").style.display = "block";
+	}else if(stc=="book"){
+		document.getElementById("book").style.display = "block";
+	}
+
+}
